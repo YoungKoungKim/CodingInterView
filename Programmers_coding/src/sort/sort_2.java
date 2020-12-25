a8a6fc742dff37c93663fe131c949ec6226d4ff7
YoungKoungKim <0koung.kim@gmail.com>
2020-12-25 20:46:47 +0900

    2020.12.25 정렬 2번 임시 commit

---
+++ Programmers_coding/src/sort/sort_2.java
@@ -0,0 +1,17 @@
+package sort;
+
+public class sort_2 {
+	 public String solution(int[] numbers) {
+	 String answer = "";
+	 
+	 String[] num = new String[numbers.length];
+	 for (int i = 0; i < numbers.length; i++) {
+		 num[i] = numbers.toString();
+	 }
+	 
+	 
+	 
+	 return answer;
+	 }
+
+}
