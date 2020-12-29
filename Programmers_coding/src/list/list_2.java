6b647bfdab0df933863425b5cd1192cd787879d4
YoungKoungKim <0koung.kim@gmail.com>
2020-12-29 23:26:18 +0900

    2020.12.29 일단커밋하고보자!!!

---
+++ Programmers_coding/src/list/list_2.java
@@ -0,0 +1,14 @@
+package list;
+
+public class list_2 {
+    public static int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+    	int[] progresses = new int[3];
+    	int[] speeds = new int[3];
+    	solution(progresses, speeds);
+	}
+}
