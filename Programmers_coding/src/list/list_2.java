e0d17e47c9c2dea044dc59b4b7985fab0ece0dbd
YoungKoungKim <0koung.kim@gmail.com>
2020-12-29 23:52:29 +0900

    2020.12.29 기능개발 중간 commit

--- Programmers_coding/src/list/list_2.java
+++ Programmers_coding/src/list/list_2.java
@@ -1,8 +1,41 @@
 package list;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class list_2 {
     public static int[] solution(int[] progresses, int[] speeds) {
         int[] answer = {};
+        
+        Queue<HashMap<String, Integer>> q = new LinkedList<>();
+        for (int i = 0; i < progresses.length; i++) {
+        	HashMap<String, Integer> map = new HashMap<>();
+        	map.put("progress", progresses[i]);
+        	map.put("speed", speeds[i]);
+        	q.add(map);
+        }
+        
+        while (!q.isEmpty()) {
+        	for (HashMap<String, Integer> temp : q) {
+        		int tProgress = temp.get("progress");
+        		int tSpeed = temp.get("speed");
+        		tProgress = tProgress + tSpeed;
+        		
+        		temp.put("progress", tProgress);
+        	}
+        	
+        	int push = 0;
+        	while (q.peek().get("progress") >= 100) {
+        		HashMap<String, Integer> resultMap = q.poll();
+        		System.out.println(resultMap.toString());
+        		push++;
+        	}
+        	
+        	System.out.println(push);
+        	System.out.println(q.toString());
+        }
+        
         return answer;
     }
     
