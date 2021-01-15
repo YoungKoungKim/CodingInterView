39ccbd08fdd31fbcb7fe3a672c753599b0a8e558
YoungKoungKim <0koung.kim@gmail.com>
2021-01-15 14:00:29 +0900

    Create Solution_210115.java

---
+++ Programmers_coding/src/practice/Solution_210115.java
@@ -0,0 +1,17 @@
+class Solution_210115 {
+    public String solution(String[] seoul) {
+        String answer = "";
+        String location = "";
+        
+        for (int i = 0; i < seoul.length; i++) {
+            if ("Kim".equals(seoul[i])) {
+                location = String.valueOf(i);
+                break;
+            }
+        }
+        
+        answer = "김서방은 " + location + "에 있다";
+        
+        return answer;
+    }
+}
