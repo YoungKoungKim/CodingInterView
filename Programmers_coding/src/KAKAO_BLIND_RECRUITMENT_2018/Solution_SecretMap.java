757c9de432b98040d6361441dd1393a07faca056
YoungKoungKim <0koung.kim@gmail.com>
2021-01-23 02:13:53 +0900

    비밀지도 테케 2개 예외처리 전 커밋

--- Programmers_coding/src/KAKAO_BLIND_RECRUITMENT_2018/Solution_SecretMap.java
+++ Programmers_coding/src/KAKAO_BLIND_RECRUITMENT_2018/Solution_SecretMap.java
@@ -2,13 +2,33 @@ package KAKAO_BLIND_RECRUITMENT_2018;
 
 public class Solution_SecretMap {
     public static void main(String[] args) {
-		
+		Solution sol = new Solution();
+		int n = 1;
+		int[] arr1 = {0};
+		int[] arr2 = {0};
+		sol.solution(n, arr1, arr2);
 	}
 }
 
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
+        String[] answer = new String[n];
+        String format = "%0" + n + "d";
+        
+        for (int i = 0; i < n; i++) {
+        	//map[i] = decToBin(n, arr1[i]);
+        	String binaryString1 = String.format(format, Integer.parseInt(Integer.toBinaryString(arr1[i]).toString()));
+        	String binaryString2 = String.format(format, Integer.parseInt(Integer.toBinaryString(arr2[i]).toString()));
+        	
+        	answer[i] = "";
+        	for (int j = 0; j < n; j++) {
+        		if (binaryString1.charAt(j) == '1' || binaryString2.charAt(j) == '1') {
+        			answer[i] = answer[i].concat("#");
+        		} else {
+        			answer[i] = answer[i].concat(" ");
+        		}
+        	}
+        }
         return answer;
     }
 }
