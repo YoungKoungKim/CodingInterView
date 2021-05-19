1cd4f1c4da8268d2f099d61eaee902bcc29524f3
YoungKoungKim <ykk0413@gmail.com>
2021-05-19 18:36:25 +0900

    HackerRank Java String Regex commit (complete)

---
+++ HackerRank_coding/src/JavaRegex.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class JavaRegex{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+    }
+}
+
+//Write your code here
+class MyRegex {
+
+	public static final String pattern = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+	
+}
