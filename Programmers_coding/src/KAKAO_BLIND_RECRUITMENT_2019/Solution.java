febfc90b4772ecca406e5787820bb98b92f5fe8e
YoungKoungKim <ykk0413@gmail.com>
2021-06-07 22:54:35 +0900

    오픈채팅방 init commit

---
+++ Programmers_coding/src/KAKAO_BLIND_RECRUITMENT_2019/Solution.java
@@ -0,0 +1,12 @@
+package KAKAO_BLIND_RECRUITMENT_2019;
+
+class Solution {
+    public String[] solution(String[] record) {
+        String[] answer = {};
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+		
+	}
+}
